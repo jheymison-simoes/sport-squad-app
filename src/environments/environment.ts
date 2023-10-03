593874e720811438042294f1333aa4a83a8043c1
Jheymison Simões dos Santos <47837256+jheymison-simoes@users.noreply.github.com>
2023-10-02 21:23:18 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://localhost:7173',
+  API_URL: 'https://sport-squad-api-homolog.up.railway.app',
   API_VERSION: 'api/v1'
 };
 
