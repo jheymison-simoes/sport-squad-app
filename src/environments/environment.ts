0fbfb426e8cb6f7524ef162b9f4c5778fc48100e
Jheymison Simões dos Santos <47837256+jheymison-simoes@users.noreply.github.com>
2023-10-28 18:55:27 -0300

    CHanged enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://sport-squad-api-homolog.up.railway.app',
-  API_VERSION: 'api/v1'
+  API_URL: 'https://localhost:7173',
+  API_VERSION: 'api/v1',
+  GOOGLE_CLIENT_ID: '1055240176969-4crb91r9vahhls10amq7q4c8jmtr7uhs.apps.googleusercontent.com'
 };
 
 /*
